8d5b160e584b02191c219a52ecfd85baef21e8b7
Harsh Garg <harshgarg1632000@gmail.com>
2023-04-13 15:51:30 +0530

    App.java

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,13 +1,27 @@
 package com;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.controller.UserController;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("Spring.xml"); 
+		System.out.println(container);
+		
+		UserController uc1 = container.getBean("userController", UserController.class);
+		UserController uc2 = container.getBean("userController", UserController.class);
+		UserController uc3 = container.getBean("userController", UserController.class);
+		 
+		System.out.println(uc1);
+		System.out.println(uc2);
+		System.out.println(uc3);
+		
+		System.out.println(uc1.getName());
+		System.out.println(uc1.getSalary());
+		
+		
+		container.registerShutdownHook();
+	}
 }
--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -35,25 +35,6 @@ public class UserController implements InitializingBean, DisposableBean{
 	public void setSalary(Integer salary) {
 		this.salary = salary;
 	}
-	
-	public static void main(String[] args) {
-		ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("Spring.xml"); 
-		System.out.println(container);
-		
-		UserController uc1 = container.getBean("userController", UserController.class);
-		UserController uc2 = container.getBean("userController", UserController.class);
-		UserController uc3 = container.getBean("userController", UserController.class);
-		 
-		System.out.println(uc1);
-		System.out.println(uc2);
-		System.out.println(uc3);
-		
-		System.out.println(uc1.getName());
-		System.out.println(uc1.getSalary());
-		
-		
-		container.registerShutdownHook();
-	}
 		
 	public void destroy() throws Exception {
 		System.out.println("destroy()");
